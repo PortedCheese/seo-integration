e2288601e923ae48038761fedeef1d500f1cfd5b
Vladimir Peshkov <vladimirpeshkov5@gmail.com>
2019-04-18 14:44:19 +0300

    disable edit inline form

--- src/resources/views/admin/meta/update.blade.php
+++ src/resources/views/admin/meta/update.blade.php
@@ -11,94 +11,93 @@
             <input type="hidden" name="_method" value="PUT">
             <input type="hidden" name="model" value="{{ $hasModel ? 'on' : 'off' }}">
             <input type="hidden" name="back" value="{{ $back }}">
-            <div class="form-row align-items-center">
-                @if (!$hasModel)
-                    <div class="col-auto">
-                        <label for="page" class="sr-only">Page</label>
-                        <input type="text"
-                               id="page"
-                               name="page"
-                               value="{{ old('page') ? old('page') : $meta->page }}"
-                               {{--required--}}
-                               placeholder="Page"
-                               class="form-control mb-2{{ $errors->has('page') ? ' is-invalid' : '' }}">
-                        @if ($errors->has('page'))
-                            <span class="invalid-feedback" role="alert">
+            @if (!$hasModel)
+                <div class="form-group">
+                    <label for="page">Page</label>
+                    <input type="text"
+                           id="page"
+                           name="page"
+                           value="{{ old('page') ? old('page') : $meta->page }}"
+                           {{--required--}}
+                           placeholder="Page"
+                           class="form-control mb-2{{ $errors->has('page') ? ' is-invalid' : '' }}">
+                    @if ($errors->has('page'))
+                        <span class="invalid-feedback" role="alert">
                                 <strong>{{ $errors->first('page') }}</strong>
                             </span>
-                        @endif
-                    </div>
-                @endif
+                    @endif
+                </div>
+            @endif
 
-                <div class="col-auto">
-                    <label for="name" class="sr-only">Name</label>
-                    <input type="text"
-                           id="name"
-                           name="name"
-                           value="{{ old('name') ? old('name') : $meta->name }}"
-                           required
-                           placeholder="Name"
-                           class="form-control mb-2{{ $errors->has('name') ? ' is-invalid' : '' }}">
-                    @if ($errors->has('name'))
-                        <span class="invalid-feedback" role="alert">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text"
+                       id="name"
+                       name="name"
+                       value="{{ old('name') ? old('name') : $meta->name }}"
+                       required
+                       placeholder="Name"
+                       class="form-control mb-2{{ $errors->has('name') ? ' is-invalid' : '' }}">
+                @if ($errors->has('name'))
+                    <span class="invalid-feedback" role="alert">
                             <strong>{{ $errors->first('name') }}</strong>
                         </span>
-                    @endif
-                </div>
+                @endif
+            </div>
 
-                <div class="col-auto">
-                    <label for="content" class="sr-only">Content</label>
-                    <input type="text"
-                           id="content"
-                           name="content"
-                           value="{{ old('content') ? old('content') : $meta->content }}"
-                           required
-                           placeholder="Content"
-                           class="form-control mb-2{{ $errors->has('content') ? ' is-invalid' : '' }}">
-                    @if ($errors->has('content'))
-                        <span class="invalid-feedback" role="alert">
+            <div class="form-group">
+                <label for="content">Content</label>
+                <input type="text"
+                       id="content"
+                       name="content"
+                       value="{{ old('content') ? old('content') : $meta->content }}"
+                       required
+                       placeholder="Content"
+                       class="form-control mb-2{{ $errors->has('content') ? ' is-invalid' : '' }}">
+                @if ($errors->has('content'))
+                    <span class="invalid-feedback" role="alert">
                             <strong>{{ $errors->first('content') }}</strong>
                         </span>
-                    @endif
-                </div>
+                @endif
+            </div>
 
-                <div class="col-auto">
-                    <label for="property" class="sr-only">Property</label>
-                    <input type="text"
-                           id="property"
-                           name="property"
-                           value="{{ old('property') ? old('property') : $meta->property }}"
-                           placeholder="Property"
-                           class="form-control mb-2{{ $errors->has('property') ? ' is-invalid' : '' }}">
-                    @if ($errors->has('property'))
-                        <span class="invalid-feedback" role="alert">
+            <div class="form-group">
+                <label for="property">Property</label>
+                <input type="text"
+                       id="property"
+                       name="property"
+                       value="{{ old('property') ? old('property') : $meta->property }}"
+                       placeholder="Property"
+                       class="form-control mb-2{{ $errors->has('property') ? ' is-invalid' : '' }}">
+                @if ($errors->has('property'))
+                    <span class="invalid-feedback" role="alert">
                             <strong>{{ $errors->first('property') }}</strong>
                         </span>
-                    @endif
-                </div>
-
-                @if ($meta->parent)
-                    <div class="col-auto">
-                        <div class="form-check">
-                            <input class="form-check-input"
-                                   type="checkbox"
-                                   @if (old('separated') && !$meta->separated)
-                                           checked
-                                   @elseif ($meta->separated)
-                                           checked
-                                   @endif
-                                   id="separated"
-                                   name="separated">
-                            <label class="form-check-label" for="separated">
-                                Отдельно
-                            </label>
-                        </div>
-                    </div>
                 @endif
+            </div>
 
-                <div class="col-auto">
-                    <button type="submit" class="btn btn-success mb-2">Обновить</button>
+            @if ($meta->parent)
+                <div class="form-group">
+                    <div class="form-check">
+                        <input class="form-check-input"
+                               type="checkbox"
+                               @if (old('separated') && !$meta->separated)
+                               checked
+                               @elseif ($meta->separated)
+                               checked
+                               @endif
+                               id="separated"
+                               name="separated">
+                        <label class="form-check-label" for="separated">
+                            Отдельно
+                        </label>
+                    </div>
                 </div>
+            @endif
+
+            <div class="form-group">
+                <button type="submit" class="btn btn-success mb-2">Обновить</button>
+                <a href="{{ $back }}" class="btn btn-dark mb-2">Назад</a>
             </div>
         </form>
     </div>
